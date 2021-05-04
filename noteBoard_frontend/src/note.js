9c72e19227b5d2814a8d8dc870e84413615e2220
SuperPox <calebkohl@gmail.com>
2021-05-03 23:19:13 -0600

    adds ability to delete notes

--- noteBoard_frontend/src/note.js
+++ noteBoard_frontend/src/note.js
@@ -6,7 +6,19 @@ function appendNotes(notes, element)
     for (let note of notes) 
     {
         const noteLi = document.createElement("li")
+
+        const noteDelete = document.createElement("button")
+        noteDelete.innerText = "Delete"
+
         noteLi.innerText = note.content
+        noteDelete.addEventListener('click', (e) => deleteNote(note.id))
+        noteLi.append(noteDelete)
         ul.append(noteLi)
     }
+}
+
+function deleteNote(noteID)
+{
+    console.log(noteID)
+    fetch(`http://localhost:3000/notes/${noteID}`, {method: "DELETE"})
 }
