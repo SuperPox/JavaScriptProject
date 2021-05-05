2dc00c6ba0a7daa1b70eb44afbb1ab3e5916044e
SuperPox <calebkohl@gmail.com>
2021-05-04 20:23:29 -0600

    incredibly convulted way to build grid

--- noteBoard_frontend/src/board.js
+++ noteBoard_frontend/src/board.js
@@ -2,7 +2,70 @@
 // VARS
 const boardForm = document.getElementById('boardForm')
 
-//
+
+
+//////////////////////////////////////////////////////////////////
+// Vars
+//const fill = document.querySelector('.fill');
+var fill = document.querySelector('.fill');
+const fills = document.querySelectorAll('.fill');
+
+const empties = document.querySelectorAll('.empty');
+const emptiesArr = []
+
+
+// Fill Listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop through Fills
+fills.forEach( f => {
+    f.addEventListener('dragstart', (e) => {
+        if (e.target.matches("div")) {
+            fill = e.target
+        }
+        })
+})
+
+// Loop through Empties
+for (const empty of empties)
+{
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+// Drag Functions
+function dragStart()
+{
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
+function dragEnd()
+{
+    this.className = 'fill';
+}
+function dragOver(e)
+{
+    e.preventDefault();
+}
+function dragEnter(e)
+{
+    e.preventDefault();
+    this.className += ' hovered';
+}
+function dragLeave()
+{
+    this.className = 'empty';
+}
+function dragDrop()
+{
+    this.className = 'empty';
+    this.append(fill);
+}
+
+///////////////////////////////////////////////////////////////////
 
 function fetchBoards()
 {
@@ -16,10 +79,17 @@ function fetchBoards()
 
 function appendBoards(boards)
 { 
+    for (let e of empties)
+    {
+        emptiesArr.push(e)
+    }
+    
     for (let board of boards)
     {
         appendBoard(board)
     }
+    //console.log(emptiesArr)
+
 }
 
 function appendBoard(board) 
@@ -32,6 +102,7 @@ function appendBoard(board)
     boardDiv.append(li)
     appendNotes(board.notes, li)   
     */
+    
 
     const boardDiv = document.getElementById('boardContainer2')
 
@@ -44,6 +115,28 @@ function appendBoard(board)
     tpSpace.className = "col-sm gridTP"
     tpSpace.innerText = "XXXXX"
     boardDiv.append(tpSpace)
+
+    //
+
+    /*
+    const emptyDiv = document.getElementById('emptyDiv')
+    const fillDiv  = document.createElement('div')
+    fillDiv.className = "fill"
+    fillDiv.innerText = board.name
+    emptyDiv.append(fillDiv)
+    
+
+    const nextDiv = emptiesArr[0]
+    nextDiv.className = "fill"
+    nextDiv.draggable = "true"
+    nextDiv.innerText = board.name
+    emptiesArr.shift
+    */
+    
+
+
+
+
 
 }
 
